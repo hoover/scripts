feeadde677bc62df67f60525e4d2df1fff54b6cb
Alex Morega <alex@grep.ro>
2016-11-04 09:42:45 +0200

    write index json files

---
+++ mof_index.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timezone
+from pathlib import Path
+import json
+
+def chunked(items, chunksize=100):
+    iterator = iter(items)
+    while True:
+        buffer = []
+        try:
+            for _ in range(chunksize):
+                buffer.append(next(iterator))
+        except StopIteration:
+            if not buffer:
+                return
+        yield buffer
+
+def write_index(index_file, prev, chunk):
+    timestamp = datetime.utcnow().replace(tzinfo=timezone.utc)
+    data = {
+        'timestamp': timestamp.isoformat(),
+        'documents': {
+            doc.stem: timestamp.isoformat()
+            for doc in chunk
+        },
+    }
+    if prev:
+        data['next'] = str(prev)
+    with index_file.open('w', encoding='utf-8') as f:
+        json.dump(data, f, sort_keys=True, indent=2)
+
+def main(repo):
+    mofs = repo / 'mofs'
+    indexdir = repo / 'index'
+    filename = lambda n: indexdir / '{}.json'.format(n)
+    for i, chunk in enumerate(chunked(sorted(mofs.iterdir())), 1):
+        prev = filename(i - 1).relative_to(index.parent) if i > 1 else None
+        index = filename(i)
+        write_index(index, prev, chunk)
+
+if __name__ == '__main__':
+    import sys
+    main(Path(sys.argv[1]))
