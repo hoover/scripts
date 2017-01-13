58faa94743e6a60d651f1c2cc412d1059af392a1
Alex Morega <alex@grep.ro>
2017-01-13 17:11:38 +0200

    read message from pffexport; add attachments

---
+++ pffexport_to_eml.py
@@ -0,0 +1,33 @@
+import sys
+from pathlib import Path
+import email
+import email.message
+import email.encoders
+from email.mime.base import MIMEBase
+
+def convert_email(folder, output):
+    with (folder / 'InternetHeaders.txt').open('rb') as f:
+        raw_headers = f.read()
+
+    message = email.message_from_bytes(raw_headers)
+    del message['Content-Type']
+    message.set_payload(None)
+    message['Content-Type'] = 'multipart/mixed'
+
+    for p in (folder / 'Attachments').iterdir():
+        attachment = MIMEBase('application', 'octet-stream')
+        with p.open('rb') as f:
+            attachment.set_payload(f.read())
+        email.encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition',
+            'attachment', filename=p.name)
+        message.attach(attachment)
+
+    output.write(message.as_bytes())
+
+if __name__ == '__main__':
+    [folder] = sys.argv[1:]
+    from io import BytesIO
+    output = BytesIO()
+    convert_email(Path(folder), output)
+    print(output.getvalue().decode('latin1'))
